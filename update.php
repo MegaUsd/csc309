96f586b3bf949b6225926ab8fe6424c494a6d242
MegaUsd <139884619+MegaUsd@users.noreply.github.com>
2023-07-18 19:49:13 +0100

    Update update.php

--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 <?php
     $server = 'localhost'; // 127.0.0.1
     $username = 'root';
-    $password = 'rootroot';
+    $password = '';
     $db = 'csc309';
 
     // Open a new connection
@@ -14,7 +14,7 @@
     }
 
     // Query
-    $sql = "UPDATE users SET firstname = 'Iruoma' WHERE id = 3";
+    $sql = "UPDATE users SET date_of_birth = '1992-05-13' WHERE id = 1";
 
     $result = $con->query($sql);
 
